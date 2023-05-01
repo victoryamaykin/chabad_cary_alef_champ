b73c169ad1c5eb5f3e298deb2ac83422269ddc2d
Victor Yamaykin <victor.yamaykin@gmail.com>
2023-05-01 11:47:22 -0400

    Update streamlit_app.py
    password protected

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import re
+from pw import pw
 
 # Load the data
 df = pd.read_csv('hebrew_school_data.csv')
@@ -16,31 +17,38 @@ class_df = df[df['teacher'] == teacher_name]
 # Create a dropdown menu for the students in that class
 student_name = st.sidebar.selectbox('Select a student', class_df['student_name'].values)
 
-# Create a form
-with st.sidebar.form('update_df'):
-    
-    st.write(f"Update progress for {student_name}")
-    
-    # Get the level 
-    level = st.radio('Level', options = ["White", "Red", "Orange", "Yellow", "Green", "Blue"])
-    
-    # Get the stripe
-    stripe = st.radio('Stripe', options = [1,2,3])
-    
-    # student homework
-    homework_1 = st.text_input(f"Enter homework for {student_name}")
-    
-    # parent homework
-#     homework_2 =  st.text_input("Enter homework for parent")
-    
-    submitted = st.form_submit_button("Submit")
-    
-    # Update the dataframe
-    if submitted:
-        df.loc[df['student_name'] == student_name, 'level'] = level
-        df.loc[df['student_name'] == student_name, 'stripe'] = stripe
-        df.loc[df['student_name'] == student_name, 'homework_1'] = homework_1
-        df.loc[df['student_name'] == student_name, 'homework_2'] = homework_2
+password = st.text_input('Enter password to update')
+
+# If the password is corect, show the update form
+if password == pw:
+
+    # Create a form
+    with st.sidebar.form('update_df'):
+
+        st.write(f"Update progress for {student_name}")
+
+        # Get the level 
+        level = st.radio('Level', options = ["White", "Red", "Orange", "Yellow", "Green", "Blue"])
+
+        # Get the stripe
+        stripe = st.radio('Stripe', options = [1,2,3])
+
+        # student homework
+        homework_1 = st.text_input(f"Enter homework for {student_name}")
+
+        # parent homework
+    #     homework_2 =  st.text_input("Enter homework for parent")
+
+        submitted = st.form_submit_button("Submit")
+
+        # Update the dataframe
+        if submitted:
+            df.loc[df['student_name'] == student_name, 'level'] = level
+            df.loc[df['student_name'] == student_name, 'stripe'] = stripe
+            df.loc[df['student_name'] == student_name, 'homework_1'] = homework_1
+            df.loc[df['student_name'] == student_name, 'homework_2'] = homework_2
+else:
+    st.write('Incorrect password. Please contact database admin Victor Yamaykin.'
 
 # Display the student's information
 st.write('**Class Information**')
