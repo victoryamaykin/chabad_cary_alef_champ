1cbf17ae05c0774d5d098ac13bc0a32bcd1671ae
Victor Yamaykin <victor.yamaykin@gmail.com>
2023-05-05 11:05:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,8 +56,10 @@ if password == pw:
 
 # Display the student's information
 st.write('**Class Information**')
-st.write(f'###**Teacher Name:** {teacher_name}')
+st.write('***********'
+st.write(f'**Teacher Name:** {teacher_name}')
+ st.write('***********'
 
 # Display the students
-st.write('**Student Levels**')
+st.write('**Chabad of Cary: Alef Champ**')
 st.table(df[df['teacher'] == teacher_name][['student_name', 'level', 'stripe', 'homework_1']])
