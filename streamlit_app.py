2149a7bfa793844378bfe7e4c1d9f24bf98754f3
Victor Yamaykin <victor.yamaykin@gmail.com>
2023-05-05 11:19:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,5 +65,4 @@ st.write(f'**Teacher Name:** {teacher_name}')
 st.write('***********')
 
 # Display the students
-st.table(df[df['teacher'] == teacher_name][['student_name', 'level', 'stripe', 'homework_1']] \
-                                            .sort_values(by=['level', 'stripe', 'name']))
+st.table(df[df['teacher'] == teacher_name][['student_name', 'level', 'stripe', 'homework_1']].sort_values(by=['level', 'stripe', 'name']))
